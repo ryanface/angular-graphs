7ca61467958fc49f67b5555ee173dbff3cc5b42c
ryanface <ryanface_metin2@yahoo.com.br>
2017-10-10 20:08:31 +0000

    fix

--- src/app/app.service.ts
+++ src/app/app.service.ts
@@ -5,27 +5,25 @@ import { Observable } from 'rxjs/Observable';
 @Injectable()
 export class AppService {
 
+  private host:string = 'http://54.167.230.31/php-api/';
+
   constructor(private http: Http) {
   }
   json(de: string, ate: string): Observable<Response> {
-    const url = 'http://php-newtxt.rhcloud.com/?param1=data&valor1=2017&param2=data&valor2='+de+'&param3=data&valor3='+ate; //&param2=doenca&valor2=a
-    //const url = 'http://192.168.211.128/php/index.php?param1=data&valor1=2017&param2=data&valor2='+de+'&param3=data&valor3='+ate; //&param2=doenca&valor2=a
+    const url = this.host+'?param1=data&valor1=2017&param2=data&valor2='+de+'&param3=data&valor3='+ate; //&param2=doenca&valor2=a
     return this.http.get(url);
   }
   jsonB(de: string, ate: string): Observable<Response> {
-    const url = 'http://php-newtxt.rhcloud.com/bairros.php?param1=data&valor1=2017&param2=data&valor2='+de+'&param3=data&valor3='+ate; //&param2=doenca&valor2=a
-    //const url = 'http://192.168.211.128/php/bairros.php?param1=data&valor1=2017&param2=data&valor2='+de+'&param3=data&valor3='+ate; //&param2=doenca&valor2=a
+    const url = this.host+'bairros.php?param1=data&valor1=2017&param2=data&valor2='+de+'&param3=data&valor3='+ate; //&param2=doenca&valor2=a
     return this.http.get(url);
   }
   jsonR(de: string, ate: string): Observable<Response> {
-    const url = 'http://php-newtxt.rhcloud.com/rosca.php?param1=data&valor1=2017&param2=data&valor2='+de+'&param3=data&valor3='+ate; //&param2=doenca&valor2=a
-    //const url = 'http://192.168.211.128/php/rosca.php?param1=data&valor1=2017&param2=data&valor2='+de+'&param3=data&valor3='+ate; //&param2=doenca&valor2=a
+    const url = this.host+'rosca.php?param1=data&valor1=2017&param2=data&valor2='+de+'&param3=data&valor3='+ate; //&param2=doenca&valor2=a
     return this.http.get(url);
   }
   list(de: string, ate: string, field: string, limit: string): Observable<Response> {
     if(limit == undefined)limit='3';
-    const url = 'http://php-newtxt.rhcloud.com/list.php?param1=data&valor1=2017&param2=data&valor2='+de+'&param3=data&valor3='+ate+'&param4='+field+'&limit='+limit; //&param2=doenca&valor2=a
-    //const url = 'http://192.168.211.128/php/list.php?param1=data&valor1=2017&param2=data&valor2='+de+'&param3=data&valor3='+ate+'&param4='+field+'&limit='+limit; //&param2=doenca&valor2=a
+    const url = this.host+'list.php?param1=data&valor1=2017&param2=data&valor2='+de+'&param3=data&valor3='+ate+'&param4='+field+'&limit='+limit; //&param2=doenca&valor2=a
     return this.http.get(url);
   }
 }
