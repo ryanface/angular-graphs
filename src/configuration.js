5bf464a53e44c03da5686872d527b4c4a65a48ac
cristianoboell <cristianoboell@gmail.com>
2018-06-02 00:18:58 +0000

    ADD final

--- src/configuration.js
+++ src/configuration.js
@@ -1,6 +1,6 @@
 var configuration = {
-    'soquet':'http://www:4100',
-    //'soquet':'http://http://ec2-54-167-230-31.compute-1.amazonaws.com:4100',    
+    //'soquet':'http://www:4100',
+    'soquet':'http://http://ec2-54-167-230-31.compute-1.amazonaws.com:4100',    
     'transports': ['websocket', 'polling']
 };
 
