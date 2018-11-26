a371bbc67f41f280bda0e5d90f271cf5f4532190
ryanface <ryanface_metin2@yahoo.com.br>
2018-11-26 15:08:30 -0800

    FIX filters

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -5,6 +5,8 @@ declare var dc: any;
 declare var crossfilter: any;
 declare var lysenkoIntervalTree: any;
 declare var intervalTreeGroup: any;
+declare var remove_empty_bins: any;
+
 var configuration = require('../../configuration');
 
 import { Component, OnInit } from '@angular/core';
@@ -23,6 +25,7 @@ export class DashboardComponent implements OnInit {
   public socket:any;
   public list:any[];
   public ndx:any;
+  public ndxTot:any;
   public filterAll:any;
   public timeline:any;
   public timescore:any;
@@ -40,14 +43,14 @@ export class DashboardComponent implements OnInit {
 
 
       this.socket.on('getCasos', (a) =>{ this.list = a; this.full(a); });
-      this.socket.on('getScores', (a,b,c) =>{ this.list = b; this.full(b); console.log('getScores',a,b,c);  });
+      this.socket.on('getScores', (a,b,c) =>{ this.list = b; this.full(b);  });
 
       if(!this.appService.checkLogin())
         this.route.navigate(['/modelo'])
       else{
-        //this.timeline     = dc.barChart("#timeline");
+        this.timeline     = dc.barChart("#timeline");
         this.barchart     = dc.pieChart("#barchart");
-        this.barUnidades  = dc.rowChart("#barUnidades");
+        //this.barUnidades  = dc.rowChart("#barUnidades");
 
         this.socket.on('MongoDB', (a) =>{ if(a == 'ok') this.get();  });
       }
@@ -63,38 +66,44 @@ export class DashboardComponent implements OnInit {
      return this.appService.checkAdmin();
   }
   public get(){
-      this.socket.emit("getCasos");
-      //this.socket.emit("getScores");
+      //this.socket.emit("getCasos");
+      this.socket.emit("getScores");
   }
   public filter(experiments:any):any{
+      var experiment:any = [];
+      console.log('experiments',experiments[0])
       experiments.forEach(function(d) {
-           let tmp = d.dataRegistro.split(' ')[0].split('/');
-           d.dataRegistro = tmp[2]+'/'+tmp[1]+'/'+tmp[0];
-           d.month       = new Date(d.dataRegistro).getMonth();
-           d.year        = new Date(d.dataRegistro).getFullYear();
-           d.open        = d.dataRegistro;
-
-           d.min    = new Date(new Date(d.dataRegistro).getTime());
-           d.max    = new Date(new Date(d.dataRegistro).getTime());
-           d.interval = [new Date(d.dataRegistro).getTime(),new Date(d.dataRegistro).getTime()];
-           //sum
-
+                 let tmp = d.data.split('T')[0].split('-');
+                 d.dataRegistro = tmp[2]+'/'+tmp[1]+'/'+tmp[0];
+                 d.month       = new Date(d.data).getMonth();
+                 d.year        = new Date(d.data).getFullYear();
+                 d.open        = d.dataRegistro;
+                 d.min         = new Date(new Date(d.data).getTime());
+                 d.max         = new Date(new Date(d.data).getTime());
+                 d.interval = [new Date(d.data).getTime(),new Date(d.data).getTime()];
+                 //sum
+                 d.Dengue      = (d.doenca == 'Dengue')      ? d.sum : 0;
+                 d.Chikungunya = (d.doenca == 'Chikungunya') ? d.sum : 0;
+                 d.Zika        = (d.doenca == 'Zika vírus')  ? d.sum : 0;
+                 d.Sifilis     = (d.doenca == 'Sífilis')     ? d.sum : 0;
+                 experiment.push(d);
       });
-      return experiments;
+      return experiment;
   }
   //
   public full(experiments:any){
       experiments   = this.filter(experiments);
-      console.log('experiments',experiments[0])
-      this.ndx = crossfilter(experiments);
-      this.filterAll = this.ndx.groupAll();
-      var firstDate = d3.min(experiments, function(x) { return (x != NaN) ? x['min'] :0; }),
-          lastDate  = d3.max(experiments, function(x) { return (x != NaN) ? x['max'] :0; });
-      //console.log(firstDate,lastDate,'experiments',experiments);
-      //TIMELINE ------------------------------------------------------
+      this.ndx      = crossfilter(experiments);
+
+
+  //TIMELINE ------------------------------------------------------
+  this.filterAll = this.ndx.groupAll();
+  var firstDate = d3.min(experiments, function(x) { return (x != NaN) ? x['min'] :0; }),
+      lastDate  = d3.max(experiments, function(x) { return (x != NaN) ? x['max'] :0; });
       var intervalDimension    = this.ndx.dimension(function(d) {return d.interval;});
       var projectsPerMonthTree = this.ndx.groupAll().reduce(
           function(v, d) {
+              //console.log('projectsPerMonthTree',d)
               v.insert(d.interval);
               return v;
           },
@@ -109,103 +118,58 @@ export class DashboardComponent implements OnInit {
               return lysenkoIntervalTree(null);
           }
       );
-
-      var projectsPerMonthGroup = intervalTreeGroup(projectsPerMonthTree.value(), firstDate, lastDate);
-      //console.log(projectsPerMonthGroup)
-      var hits = intervalDimension.group().reduceSum(dc.pluck('month'));
-     //console.log(dateDim)
-     //MULT------------------------------------------------------
-
-         var composite = dc.compositeChart(d3.select('#timeline'));
-         composite
+        var filtered_projectsPerMonth = remove_empty_bins(projectsPerMonthTree)
+      console.log(filtered_projectsPerMonth)
+      var projectsPerMonthGroup = intervalTreeGroup(projectsPerMonthTree.value(), firstDate, lastDate, experiments);
+      this.timeline
              .width(1000)
              .height(300)
-             .x(d3.scaleTime())
-             .y(d3.scaleLinear().domain([0,200]))
+             .y(d3.scaleLinear().domain([0,50]))
+             .x(d3.scaleTime().domain([new Date(2000, 0, 1), new Date(2020, 11, 31)]))
              .renderHorizontalGridLines(true)
-             .xUnits(d3.timeMonths)
+             .xUnits(d3.timeDays)
+             .ordinalColors(['red','blue'])
+             .gap(5)
              .elasticX(true)
              .brushOn(true)
              .yAxisLabel("Número de Casos")
              .xAxisLabel("Meses")
-             .compose([
-               dc.lineChart(composite)
-                   .width(1000)
-                   .height(300)
-                   .ordinalColors(['blue','orange'])
-                   .renderLabel(true)
-                   .dimension(intervalDimension)
-                   .group(projectsPerMonthGroup),
-               dc.lineChart(composite)
-                   .width(1000)
-                   .height(300)
-                   .ordinalColors(['green','blue','orange'])
-                   .renderLabel(true)
-                   .dimension(intervalDimension)
-                   .group(projectsPerMonthGroup),
-               dc.barChart(composite)
-                      .width(1000)
-                      .height(300)
-                      .elasticX(true)
-                      .ordinalColors(['red','green','blue','orange'])
-                      .brushOn(true)
-                      .yAxisLabel("Número de Casos")
-                      .xAxisLabel("Meses")
-                      .renderLabel(true)
-                      .dimension(intervalDimension)
-                      .group(projectsPerMonthGroup)
-                      .controlsUseVisibility(true)
-                      .filterHandler(function(dim, filters) {
-                                  if(filters && filters.length) {
-                                      if(filters.length !== 1)
-                                          throw new Error('not expecting more than one range filter');
-                                      var range = filters[0];
-                                      dim.filterFunction(function(i) {
-                                          return !(i[1] < range[0].getTime() || i[0] > range[1].getTime());
-                                      })
-                                  }
-                                  else dim.filterAll();
-                                  return filters;
-                           })
-             ])
+             .renderLabel(true)
+             .dimension(intervalDimension)
+             .group(projectsPerMonthGroup)
+             .controlsUseVisibility(true);
+             this.timeline.filterHandler(function(dim, filters) {
+                   if(filters && filters.length) {
+                       if(filters.length !== 1)
+                           throw new Error('not expecting more than one range filter');
+                       var range = filters[0];
+                       dim.filterFunction(function(i) {
+                           return !(i[1] < range[0].getTime() || i[0] > range[1].getTime());
+                       })
+                   }
+                   else dim.filterAll();
+                   return filters;
+            });
 
-//----------------------------------------------
-var data = [
-   {date: "12/27/2012", http_404: 2, http_200: 190, http_302: 100,total:0},
-   {date: "12/28/2012", http_404: 2, http_200: 10, http_302: 100,total:0},
-   {date: "12/29/2012", http_404: 1, http_200: 300, http_302: 200,total:0},
-   {date: "12/30/2012", http_404: 2, http_200: 90, http_302: 0,total:0},
-   {date: "12/31/2012", http_404: 2, http_200: 90, http_302: 0,total:0},
-   {date: "01/01/2013", http_404: 2, http_200: 90, http_302: 0,total:0},
-   {date: "01/02/2013", http_404: 1, http_200: 10, http_302: 1,total:0},
-   {date: "01/03/2013", http_404: 2, http_200: 90, http_302: 0,total:0},
-   {date: "01/04/2013", http_404: 2, http_200: 90, http_302: 0,total:0},
-   {date: "01/05/2013", http_404: 2, http_200: 90, http_302: 0,total:0},
-   {date: "01/06/2013", http_404: 2, http_200: 200, http_302: 1,total:0},
-   {date: "01/07/2013", http_404: 1, http_200: 200, http_302: 100,total:0}
-   ];
-    var ndx = crossfilter(data);
-    var parseDate = d3.timeParse("%m/%d/%Y");
+  //SCORES ------------------------------------------------------
+    var dateDim         = this.ndx.dimension(function(d) {return +d.max;});
 
-    data.forEach(function(d) {
-     d.date = parseDate(d.date);
-     d.total= d.http_404+d.http_200+d.http_302;
-    });
-    console.log(data);
+    var Dengue          = dateDim.group().reduceSum(dc.pluck('Dengue'));
+    var filtered_Dengue = remove_empty_bins(Dengue)
+    var Chikungunya     = dateDim.group().reduceSum(dc.pluck('Chikungunya'));
+    var filtered_Chikungunya = remove_empty_bins(Chikungunya)
+    var Zika            = dateDim.group().reduceSum(dc.pluck('Zika'));
+    var filtered_Zika   = remove_empty_bins(Zika)
+    var Sifilis         = dateDim.group().reduceSum(dc.pluck('Sifilis'));
+    var filtered_Sifilis = remove_empty_bins(Sifilis)
 
-    var compositex = dc.compositeChart(d3.select('#barMulta'));
-    var dateDim = ndx.dimension(function(d) {return d.date;});
-    var hits = dateDim.group().reduceSum(dc.pluck('total'));
-    var minDate = dateDim.bottom(1)[0].total;
-    var maxDate = dateDim.top(1)[0].total;
-    //console.log(minDate,maxDate);
-
-
-    compositex
+    var compositeTwo = dc.compositeChart(d3.select('#barMulta'));
+    compositeTwo
         .width(1000)
         .height(300)
-        .x(d3.scaleTime())
-        .y(d3.scaleLinear().domain([0,500]))
+        //.x(d3.scaleTime())
+        .y(d3.scaleLinear().domain([0,50]))
+        .x(d3.scaleTime().domain([new Date(2000, 0, 1), new Date(2020, 11, 31)]))
         .renderHorizontalGridLines(true)
         .xUnits(d3.timeMonths)
         .elasticX(true)
@@ -213,42 +177,41 @@ var data = [
         .yAxisLabel("Número de Casos")
         .xAxisLabel("Meses")
         .compose([
-          dc.lineChart(compositex)
+          dc.lineChart(compositeTwo)
               .width(1000)
               .height(300)
-              .renderLabel(true)
+              .ordinalColors(['red'])
               .dimension(dateDim)
-              .group(hits),
-          dc.lineChart(compositex)
+              .group(filtered_Dengue,"Dengue"),
+          dc.lineChart(compositeTwo)
               .width(1000)
               .height(300)
-              .renderLabel(true)
+              .ordinalColors(['green'])
+              .renderHorizontalGridLines(true)
               .dimension(dateDim)
-              .group(hits)
+              .group(filtered_Chikungunya,"Chikungunya"),
+          dc.lineChart(compositeTwo)
+              .width(1000)
+              .height(300)
+              .ordinalColors(['blue'])
+              .dimension(dateDim)
+              .group(filtered_Zika,"Zika"),
+          dc.lineChart(compositeTwo)
+              .width(1000)
+              .height(300)
+              .ordinalColors(['black'])
+              .renderHorizontalGridLines(true)
+              .dimension(dateDim)
+              .group(filtered_Sifilis,"Sífilis")
         ])
+
 //----------------------------------------------
-     //BAR UNIDADES------------------------------------------------------
-     var barUnidadesDimension = this.ndx.dimension(function (d) {
-         return d.unidade;
-     });
-     var barUnidadesGroup = barUnidadesDimension.group().reduceSum(function (d) {
-         return +1;//d.idUnidade;
-     });
-      this.barUnidades
-          .width(400)
-          .height(300)
-          .renderLabel(true)
-          .dimension(barUnidadesDimension)
-          .ordinalColors(['red','green','blue','orange'])
-          .group(barUnidadesGroup)
-          .turnOnControls(true)
-          .legend(dc.legend());
     //BAR DOENCAS ------------------------------------------------------
      var barchartDimension = this.ndx.dimension(function (d) {
          return d.doenca;
      });
      var barchartGroup = barchartDimension.group().reduceSum(function (d) {
-         return +1;//d.idUnidade;
+         return +d.sum;
      });
      this.barchart
          .width(400)
@@ -257,13 +220,11 @@ var data = [
          .innerRadius(100)
          .externalLabels(50)
          .externalRadiusPadding(100)
-         .ordinalColors(['red','green','blue','orange'])
+         .ordinalColors(['red','green','blue','black'])
          .drawPaths(true)
          .dimension(barchartDimension)
          .group(barchartGroup)
          .legend(dc.legend());
-
-
 
       dc.renderAll();
   }
