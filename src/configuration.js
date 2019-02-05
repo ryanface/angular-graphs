c552fa101903cb26246ba6aa7a22ad92ec53a3dc
MarciaBoell <marcia.boell@gmail.com>
2019-02-05 13:48:51 -0800

    FIX settings

--- src/configuration.js
+++ src/configuration.js
@@ -1,6 +1,6 @@
 var configuration = {
     //'socket':'http://www:4100',
-    'socket':'ec2-52-90-245-155.compute-1.amazonaws.com:4100',
+    'socket':'http://ec2-54-197-36-53.compute-1.amazonaws.com:4100',
     'transports': ['websocket', 'polling'],
 };
 
