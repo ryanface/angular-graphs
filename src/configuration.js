7e71ac169accf6943625f2160a6241235023ef88
cristiano <ryanface_metin2@yahoo.com.br>
2019-05-13 00:07:48 +0000

    FIX legend

--- src/configuration.js
+++ src/configuration.js
@@ -1,6 +1,6 @@
 var configuration = {
-    //'socket':'http://www:4100',
-    'socket':'http://http://ec2-35-175-235-0.compute-1.amazonaws.com:4300',
+    'socket':'http://www:4100',
+    //'socket':'http://ec2-35-175-235-0.compute-1.amazonaws.com:4300',
     'transports': ['websocket', 'polling'],
 };
 
